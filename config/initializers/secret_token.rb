34cdb422b18fc75b030b9499b5e3c5f338898f91
John Heimkes IV <admin@johnheimkes.com>
2010-11-29 22:21:30 -0600

    initial fucking commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gfda::Application.config.secret_token = 'b18cfc7b3a5de4bcbc28aa0ea7c40475ec318e63d427d7109d2062d70a6646774b3745f52b9eb5a49380274959cdc0241b09cbbd3b6681d0594681291c4c00e8'
